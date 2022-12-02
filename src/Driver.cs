262d65a312daf5d027ece0de2ad71e3b6daafb28
Zylve <70604613+Zylve@users.noreply.github.com>
2022-12-01 18:06:35 -0800

    Day 1

---
+++ src/Driver.cs
@@ -0,0 +1,14 @@
+namespace net.zylve.aoc {
+    public static class Driver {
+        private static void testAll() {
+            new Day1().Main1();
+            new Day1().Main2();
+        }
+        public static void Main(string[] args) {
+            if(args.Length > 0 && args[0] == "-a") {
+                testAll();
+                return;
+            }
+        }
+    }
+}
---
+++ src/day1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+
+namespace net.zylve.aoc {
+    public class Day1 {
+        private IOrderedEnumerable<int>? calories {
+            get => ReadInput().ToArray()
+                    .GroupBy(x => x.id)
+                    .Select(x => x.Sum(x => x.amount))
+                    .OrderByDescending(x => x);
+        }
+
+        private static IEnumerable<(int id, int amount)> ReadInput() {
+            int counter = 1;
+            foreach(string line in File.ReadAllLines($"input/input1.txt")) {
+                if(line?.Length == 0) {
+                    counter++;
+                } else {
+                    yield return (counter, int.Parse(line!));
+                }
+            }
+        }
+        public void Main1() {
+            Console.WriteLine($"Solution to problem one: {calories.Take(1).Sum()}");
+        }
+
+        public void Main2() {
+            Console.WriteLine($"Solution to problem two: {calories.Take(3).Sum()}");
+        }
+    }
+}
